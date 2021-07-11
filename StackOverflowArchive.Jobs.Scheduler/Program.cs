8da9b30180e7313f7aa11210ccb5b70d98eca668
Chad Golden <chad@chadgolden>
2021-07-11 14:50:44 -0400

    clean up Program.cs

--- StackOverflowArchive.Jobs.Scheduler/Program.cs
+++ StackOverflowArchive.Jobs.Scheduler/Program.cs
@@ -19,35 +19,7 @@ namespace StackOverflowArchive.Jobs.Scheduler
                     services.AddQuartz(q =>
                     {
                         q.UseMicrosoftDependencyInjectionScopedJobFactory();
-
                         q.AddConfiguredQuartzJobTriggers(hostContext.Configuration);
-
-                        //var loggingJobKey = new JobKey("LoggingJob");
-                        //q.AddJob<LoggingJob>(options => options.WithIdentity(loggingJobKey));
-                        //q.AddTrigger(options =>
-                        //{
-                        //    options.ForJob(loggingJobKey)
-                        //        .WithIdentity("LoggingJobTrigger")
-                        //        .WithCronSchedule("0/10 * * * * ?"); // every 10 seconds
-                        //});
-
-                        //var countPostJobKey = new JobKey("CountPostJob");
-                        //q.AddJob<CountPostsJob>(options => options.WithIdentity(countPostJobKey));
-                        //q.AddTrigger(options =>
-                        //{
-                        //    options.ForJob(countPostJobKey)
-                        //        .WithIdentity("CountPostJobTrigger")
-                        //        .WithCronSchedule("0/5 * * * * ?"); // every 5 seconds
-                        //});
-
-                        //var countUsersJobKey = new JobKey("CountUserJob");
-                        //q.AddJob<CountUsersJob>(options => options.WithIdentity(countUsersJobKey));
-                        //q.AddTrigger(options =>
-                        //{
-                        //    options.ForJob(countUsersJobKey)
-                        //        .WithIdentity("CountUsersJobTrigger")
-                        //        .WithCronSchedule("0/5 * * * * ?"); // every 5 seconds
-                        //});
                     });
 
                     services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
